3aeb41c57cbc2a1ba03b065ff695c3ff822fb9c3
Tom Gurka <tommy.gurka@gmail.com>
2014-06-26 22:34:28 -0700

    Initial Commit

---
+++ Week4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Week4
+//
+//  Created by Tom Gurka on 6/26/14.
+//  Copyright (c) 2014 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Week4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Week4
+//
+//  Created by Tom Gurka on 6/26/14.
+//  Copyright (c) 2014 tom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
